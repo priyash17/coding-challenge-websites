8d66aea1c53a3e0e8e39fd3e7723c73ca10060b8
Priyash Motghare <priyashmotghare@gmail.com>
2026-07-03 13:00:31 +0530

    Added solution - LeetHub

---
+++ 8041-conditional-statements-Hackerrank/8041-conditional-statements.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    if(n==1)
+    cout<<"one";
+    else if(n==2)
+    cout<<"two";
+    else if(n==3)
+    cout<<"three";
+    else if(n==4)
+    cout<<"four";
+    else if(n==5)
+    cout<<"five";
+    else if(n==6)
+    cout<<"six";
+    else if(n==7)
+    cout<<"seven";
+    else if(n==8)
+    cout<<"eight";
+    else if(n==9)
+    cout<<"nine";
+    else 
+    cout<<"Greater than 9";
+    return 0;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
