8f8ae6e6be1f43bf5c128aed60ec885b91c181db
Priyash Motghare <priyashmotghare@gmail.com>
2026-07-03 13:02:37 +0530

    Added solution - LeetHub

---
+++ 9954-arrays-introduction-Hackerrank/9954-arrays-introduction.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n,i;
+    cin>>n;
+    int a[n];
+    for(i=0;i<n;i++)
+    cin>>a[i];
+    for(i=n-1;i>=0;i--)
+    cout<<a[i]<<" ";
+    return 0;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
