ad7ee35e5176f98455368100fb1b5d75445448da
Priyash Motghare <priyashmotghare@gmail.com>
2026-07-03 13:15:57 +0530

    Added solution - LeetHub

---
+++ 67733-overloading-ostream-operator-Hackerrank/67733-overloading-ostream-operator.cpp
@@ -0,0 +1,13 @@
+
+// Enter your code here.
+ostream& operator <<(ostream& os,Person& r){
+    cout<<"first_name="<<r.get_first_name()<<","<<"last_name="<<r.get_last_name();
+    return os;
+    
+}
+
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
