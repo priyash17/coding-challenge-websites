32be192d122ee8b107d255448f2b9a89b53be8fd
Priyash Motghare <priyashmotghare@gmail.com>
2026-07-03 12:59:28 +0530

    Added solution - LeetHub

---
+++ 12103-input-and-output-Hackerrank/12103-input-and-output.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a,b,c;
+    cin>>a>>b>>c;
+    int r=a+b+c;
+    cout<<r;   
+    return 0;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
