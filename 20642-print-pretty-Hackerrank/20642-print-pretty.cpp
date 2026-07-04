44ca3eca5cfdd2ef1e68fc146d6ff3fbadea5589
Priyash Motghare <priyashmotghare@gmail.com>
2026-07-04 16:27:10 +0530

    Added solution - LeetHub

---
+++ 20642-print-pretty-Hackerrank/20642-print-pretty.cpp
@@ -0,0 +1,18 @@
+
+
+		/* Enter your code here */
+        cout << hex << left << showbase << nouppercase;
+        cout << (long long)A << endl;
+
+        cout << right << setw(15) << setfill('_') << showpos << fixed << setprecision(2);
+        cout << B << endl;
+
+        cout << scientific << uppercase << noshowpos << setprecision(9);
+        cout << C << endl;
+        
+
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
