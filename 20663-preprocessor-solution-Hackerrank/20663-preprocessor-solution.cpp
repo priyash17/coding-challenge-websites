425f3474907d4063a247ab3d2a75e394d0e12c22
Priyash Motghare <priyashmotghare@gmail.com>
2026-07-10 20:04:42 +0530

    Added solution - LeetHub

---
+++ 20663-preprocessor-solution-Hackerrank/20663-preprocessor-solution.cpp
@@ -0,0 +1,16 @@
+/* Enter your macros here */
+
+
+
+#define toStr(x) #x
+#define io(v) cin >> v
+#define FUNCTION(name, op) void name(int &a, int b) { if (b op a) a = b; }
+#define INF 100000000
+#define foreach(v, i) for (int i = 0; i < v.size(); i++)
+
+
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
